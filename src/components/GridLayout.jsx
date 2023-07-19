ae2f9eaa05c40fc55e19c3d62e4837a6f10f33a9
aizhuxue007 <aizhuprogram@gmail.com>
2023-07-19 07:24:53 -0400

    made changes to GridLayout.jsx not sure what

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -7,59 +7,16 @@ import MainInput from "./MainInput";
 import RenderAreaCharts from "./RenderAreaCharts";
 
 const GridLayout = ({ showModal }) => {
+  let goalsFromStorage = localStorage.getItem("goals");
+  let todosFromStorage = localStorage.getItem("todos")
+
   const hTagRefs = useRef([]);
   const [mainInput, setMainInput] = useState("");
 
-  const [todos, setTodos] = useState([]);
-  const [goals, setGoals] = useState([
-    {
-      id: 0,
-      time: "Two-Year Goal",
-      question: "What's the one thing I want to accomplish in two years?",
-      input: "I want to be making $100,000 a year as a full-time AWS DevOps.",
-      font: true,
-    },
-    {
-      id: 1,
-      time: "One-Year Goal",
-      question:
-        "Based on my Two Year Goal, what's the one thing I can do this year?",
-      input: "Secure a web developer role to learn and gain experience.",
-      font: false,
-    },
-    {
-      id: 2,
-      time: "Monthly Goal",
-      question:
-        "Based on my One Year Goal, what's the one thing I can do this month?",
-      input:
-        "Complete HTML/CSS, Practical Javascript and Git Like a Pro Courses.",
-      font: false,
-    },
-    {
-      id: 3,
-      time: "Weekly Goal",
-      question:
-        "Based on my Monthly Goal, what's the one thing I can do this week?",
-      input: "Finish HTML/CSS course and start Practical Javascript Course",
-      font: false,
-    },
-    {
-      id: 4,
-      time: "Daily Goal",
-      question: "Based on my Weekly Goal, what's the one thing I can do today?",
-      input: "Complete 50% of the HTML/CSS course.",
-      font: false,
-    },
-    {
-      id: 5,
-      time: "Right Now",
-      question:
-        "Based on my Daily Goal, what's the one thing I can do right now?",
-      input: "Continue on the CSS course and code along.",
-      font: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(todosFromStorage ? todosFromStorage : []);
+  const [goals, setGoals] = useState(
+    goalsFromStorage ? goalsFromStorage : []
+  );
 
   useEffect(() => {
     // Try to understand code!
@@ -68,7 +25,69 @@ const GridLayout = ({ showModal }) => {
         ref.current.textContent = goals[index].input;
       }
     });
+    if (goals === null)
+      setGoals([
+        {
+          id: 0,
+          time: "Two-Year Goal",
+          question: "What's the one thing I want to accomplish in two years?",
+          input:
+            "I want to be making $100,000 a year as a full-time AWS DevOps.",
+          font: true,
+        },
+        {
+          id: 1,
+          time: "One-Year Goal",
+          question:
+            "Based on my Two Year Goal, what's the one thing I can do this year?",
+          input: "Secure a web developer role to learn and gain experience.",
+          font: false,
+        },
+        {
+          id: 2,
+          time: "Monthly Goal",
+          question:
+            "Based on my One Year Goal, what's the one thing I can do this month?",
+          input:
+            "Complete HTML/CSS, Practical Javascript and Git Like a Pro Courses.",
+          font: false,
+        },
+        {
+          id: 3,
+          time: "Weekly Goal",
+          question:
+            "Based on my Monthly Goal, what's the one thing I can do this week?",
+          input: "Finish HTML/CSS course and start Practical Javascript Course",
+          font: false,
+        },
+        {
+          id: 4,
+          time: "Daily Goal",
+          question:
+            "Based on my Weekly Goal, what's the one thing I can do today?",
+          input: "Complete 50% of the HTML/CSS course.",
+          font: false,
+        },
+        {
+          id: 5,
+          time: "Right Now",
+          question:
+            "Based on my Daily Goal, what's the one thing I can do right now?",
+          input: "Continue on the CSS course and code along.",
+          font: false,
+        },
+      ]
+    );
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("goals", goals);
+
+  }, [goals]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", todos)
+  })
 
   let goalsToRender = goals.slice(0, -1);
 
@@ -95,7 +114,7 @@ const GridLayout = ({ showModal }) => {
       >
         {goalsToRender.map((goal, index) => (
           <>
-           {/* {console.log(goalsToRender.length)} */}
+            {/* {console.log(goalsToRender.length)} */}
             <GridItem
               key={index}
               goal={goal}
