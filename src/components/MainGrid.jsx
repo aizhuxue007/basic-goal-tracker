b3acd1629ee12b89fe4b0992017252dc58124193
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-07 21:52:26 -0400

    attempt again to fix responsiveness

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -3,7 +3,7 @@ import AppTitle from "./AppTitle";
 
 const MainGrid = ({ children }) => {
   return (
-    <div className="main-grid-item col-start-2 col-span-2 row-start-1 row-span-4 sm:col-start-auto sm:col-span-auto sm:row-start-auto sm:row-span-auto">
+    <div className="main-grid-item lg:col-start-auto lg:col-span-auto lg:row-start-auto lg:row-span-auto sm:col-start-2 sm:col-span-2 sm:row-start-1 sm:row-span-4 ">
           <div className="item-container h-full w-full rounded-3xl">
             <AppTitle />
             {children}
