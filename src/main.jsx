7ceda6eca120a2322cc3e1138aad68610ba80ac4
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-09 07:31:47 -0400

    add SignUp route

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import SignUp from './components/SignUp.jsx'
+import Register from './components/Register.jsx'
 import './css/index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />
+  },
+  {
+    path: "/register",
+    element: <Register />
   }
 ])
 
