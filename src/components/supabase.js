3392d2713597bcc1efc082d4e37a801889858dff
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-09 07:35:52 -0400

    deleted server.js file and removed auth.js content to goaltracker-server directory

--- src/components/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_PROJECT_URL = "https://vgigltsnmhcxllawoozp.supabase.co";
-const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnaWdsdHNubWhjeGxsYXdvb3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk4NzA3OTYsImV4cCI6MjAwNTQ0Njc5Nn0.wmh_PVPtPxEuxYwGbmeN-gRWhXUTGR9Koh_9f5PN3Kk";
-
-const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_API_KEY)
-
-export default supabase;
