2acb2ff99d8e9a931eb4d361b4058edcf24ef9fc
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-12 07:50:06 -0400

    re-added supabase.js to remove errors

---
+++ src/components/supabase.jsx
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js"
+
+const projectUrl = "https://vgigltsnmhcxllawoozp.supabase.co"
+const projectApi = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnaWdsdHNubWhjeGxsYXdvb3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk4NzA3OTYsImV4cCI6MjAwNTQ0Njc5Nn0.wmh_PVPtPxEuxYwGbmeN-gRWhXUTGR9Koh_9f5PN3Kk"
+
+const supabase = createClient(projectUrl, projectApi)
+
+export { supabase };
+
