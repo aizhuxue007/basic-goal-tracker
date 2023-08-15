2c0d804ddbb456487207895d756abd2aa4e4f0c8
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-15 07:09:42 -0400

    styled signinform

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 const SignInForm = () => {
   return (
-    <div className='w-4/5 h-screen my-0 mx-auto'>
-      <form className="container flex-row h-1/2 my-10 bg-green-500 p-10" action="POST">
-        <label htmlFor="email">Email</label>
-        <input id="email" className="text-white" type="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" className="text-white" type="password" />
+    <div className='w-full min-h-screen flex justify-center items-center'>
+      <form className="min-h-content  bg-green-500 p-40 rounded" action="POST">
+        <h1 className='text-white text-center text-2xl font-semibold mb-10'>Log in</h1>
         <div className="flex flex-col gap-2">
-          <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <label htmlFor="email" className='text-white'>Email</label>
+          <input id="email" className="text-white bg-green-700 rounded p-2" type="email" />
+          <label htmlFor="password" className='text-white'>Password</label>
+          <input id="password" className="text-white bg-green-700 rounded mb-10 p-2" type="password" />
+        </div>
+        
+        <div className="w-full flex flex-row space-x-4">
+          <button type='submit' className="bg-green-700 hover:bg-green-300 text-white font-bold py-2 px-4 rounded">
             Log in
           </button>
-          <a href="./register" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href="./register" className="bg-green-700 hover:bg-green-300 text-white font-bold py-2 px-4 rounded">
             Register
           </a>
         </div>
