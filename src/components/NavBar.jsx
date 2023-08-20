e89ec40b2a2da94371ebcfc5e3e856b38446101c
aizhuxue007 <aizhuprogram@gmail.com>
2023-08-20 13:07:14 -0400

    fixed navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Welcome from './Welcome'
 
 const NavBar = () => {
   return (
-    <nav className='bg-green-700 p-5 fixed w-full text-white flex item-center justify-between'>
+    <nav className='bg-green-700 p-5 sticky w-full text-white flex item-center justify-between'>
         <h1><a href="/">LOGO</a></h1>
         <Welcome />
         <ul className=''>
